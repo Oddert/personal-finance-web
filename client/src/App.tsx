828b91d3ce205227a70028d7d383449a6d491755
Robyn F H Veitch <oddert@gmail.com>
2023-06-26 21:23:13 +0100

    added basic server structure and connection test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [msg, setMsg] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:8080/')
+      .then(res => res.json())
+      .then(res => setMsg(res))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +24,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {msg}
         </a>
       </header>
     </div>
