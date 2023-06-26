10ca7a8696ef8ed9277acbad24309bbee14bfb73
Robyn F H Veitch <oddert@gmail.com>
2023-06-26 21:22:28 +0100

    added basic server structure and connection test

---
+++ server/src/app.ts
@@ -0,0 +1,31 @@
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
+
+import root from './routes/root'
+
+dotenv.config()
+
+const PORT = process.env.PORT || '8080'
+
+const app = express()
+
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms')
+)
+
+app.use(express.static('./'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(express.json());
+app.use(cors());
+
+app.use('/', root)
+
+app.listen(
+    PORT,
+    () => `[${new Date().toLocaleString('en-GB')}] Server initialised on PORT: ${PORT}`
+)
