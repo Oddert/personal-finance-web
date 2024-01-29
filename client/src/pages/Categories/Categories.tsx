009c2dfe87f12d5cab8599d35bea44d288cbe241
Robyn F H Veitch <oddert@gmail.com>
2024-01-29 20:32:27 +0000

    added utility to creat enew categories

--- client/src/pages/Categories/Categories.tsx
+++ client/src/pages/Categories/Categories.tsx
@@ -1,15 +1,23 @@
+import { useCallback, useState } from 'react'
 import { useSelector } from 'react-redux'
 
-import { Container, List, Typography } from '@mui/material'
+import { Box, Button, Container, List, ListItem, Typography } from '@mui/material'
+import { Add as PlusIcon } from '@mui/icons-material'
 
 import { Category as CategoryT } from '../../types/Category'
 
 import { getCategoryResponse } from '../../redux/selectors/categorySelectors'
 
-import Category from '../../components/Category/Category'
+import Category from '../../components/Category/'
+import CategoryAdd from '../../components/CategoryAdd/'
 
 const Categories = () => {
+    const [dialogOpen, setDialogOpen] = useState(false)
+
     const categories = useSelector(getCategoryResponse)
+
+    const handleDialogClose = useCallback(() => setDialogOpen(false), [])
+    const handleDialogOpen = useCallback(() => setDialogOpen(true), [])
     return (
         <Container
             sx={(theme) => ({
@@ -22,6 +30,17 @@ const Categories = () => {
             <Typography variant='h2' sx={{ margin: '32px 0' }}>
                 Categories
             </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    padding: '0 8px',
+                }}
+            >
+                <Button onClick={handleDialogOpen} variant='contained'>
+                    Add Category
+                </Button>
+            </Box>
             <List
                 sx={(theme) => ({
                     display: 'grid',
@@ -36,7 +55,23 @@ const Categories = () => {
                 {categories.map((category: CategoryT) => (
                     <Category category={category} key={category.id} />
                 ))}
+                <ListItem>
+                    <Button
+                        onClick={handleDialogOpen}
+                        sx={{
+                            width: '100%',
+                            height: '100%',
+                        }}
+                        variant='outlined'
+                    >
+                        <PlusIcon fontSize='large' />
+                    </Button>
+                </ListItem>
             </List>
+            <CategoryAdd
+                handleClose={handleDialogClose}
+                open={dialogOpen}
+            />
         </Container>
     )
 }
