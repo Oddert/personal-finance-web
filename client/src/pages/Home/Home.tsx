82104e0a6c6730c5c3fd03ea3f2058b3f2f8ac58
Robyn F H Veitch <oddert@gmail.com>
2024-02-04 14:41:47 +0000

    added exisitng modules to home

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import { Container, Typography } from '@mui/material'
+import { Container } from '@mui/material'
+
+import ProjectionLineChart from '../../modules/ProjectionLineChart/ProjectionLineChart'
+import ExistingDataLineChart from '../../modules/ExistingDataLineChart/ExistingDataLineChart'
 
 const Home = () => {
     return (
         <Container
             sx={{
-                height: '200vh'
+                height: '200vh',
+                display: 'grid',
+                gridTemplateColumns: '1fr 1fr',
+                alignContent: 'start',
+                // gridAutoRows: '400px',
             }}
         >
-            <Typography variant='h1'>H1 Title</Typography>
-            <Typography variant='h2'>H2 Title</Typography>
-            <Typography variant='h3'>H3 Title</Typography>
-            <Typography variant='h4'>H4 Title</Typography>
-            <Typography variant='h5'>H5 Title</Typography>
-            <Typography variant='h6'>H6 Title</Typography>
-            <Typography variant='subtitle1'>Subtitle 1</Typography>
-            <Typography variant='subtitle2'>Subtitle 2</Typography>
-            <Typography variant='body1'>Body 1</Typography>
-            <Typography variant='body2'>Body 2</Typography>
+            <ExistingDataLineChart />
+            <ProjectionLineChart compact />
         </Container>
     )
 }
