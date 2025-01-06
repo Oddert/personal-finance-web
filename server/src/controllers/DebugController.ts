4d68ce7c8a5583c14a431bca7add0afd8ecfe97d
Robyn Felicity Heather Veitch <robynfhveitch@gnail.com>
2025-01-06 15:58:38 +0000

    refactor: swapped inconcistant tabs-spaces

--- server/src/controllers/DebugController.ts
+++ server/src/controllers/DebugController.ts
@@ -32,34 +32,34 @@ export const getMatcherCategory = async (req: Request, res: Response) => {
  * Retrieves a list of all data from all tables in a format ready to be used as seed data.
  */
 export const getSeeds = async (req: Request, res: Response) => {
-	try {
-		const matchers = (await Matcher.query()).map((datum) => ({
-			...datum,
-			case_sensitive: Boolean(datum.case_sensitive),
-		}));
-		const categories = await Category.query();
-		const category_matcher = await CategoryMatcher.query();
-		const scenario = (await Scenario.query()).map((datum) => ({
-			...datum,
-			start_date: datum.start_date === '' ? null : datum.start_date,
-			end_date: datum.end_date === '' ? null : datum.end_date,
-		}));
-		const transactor = (await Transactor.query()).map((datum) => ({
-			...datum,
-			is_addition: Boolean(datum.is_addition),
-		}));
-		const scheduler = await Scheduler.query();
-		const transaction = await Transaction.query();
-		return respondOk(req, res, {
-			matchers,
-			categories,
-			category_matcher,
-			scenario,
-			transactor,
-			scheduler,
-			transaction,
-		})
-	} catch (error: any) {
-		respondServerError(req, res, null, null, 500, error.message)
-	}
+    try {
+        const matchers = (await Matcher.query()).map((datum) => ({
+            ...datum,
+            case_sensitive: Boolean(datum.case_sensitive),
+        }))
+        const categories = await Category.query()
+        const category_matcher = await CategoryMatcher.query()
+        const scenario = (await Scenario.query()).map((datum) => ({
+            ...datum,
+            start_date: datum.start_date === '' ? null : datum.start_date,
+            end_date: datum.end_date === '' ? null : datum.end_date,
+        }))
+        const transactor = (await Transactor.query()).map((datum) => ({
+            ...datum,
+            is_addition: Boolean(datum.is_addition),
+        }))
+        const scheduler = await Scheduler.query()
+        const transaction = await Transaction.query()
+        return respondOk(req, res, {
+            matchers,
+            categories,
+            category_matcher,
+            scenario,
+            transactor,
+            scheduler,
+            transaction,
+        })
+    } catch (error: any) {
+        respondServerError(req, res, null, null, 500, error.message)
+    }
 }
