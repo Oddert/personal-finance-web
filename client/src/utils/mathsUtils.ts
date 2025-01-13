5b4ee7a1b62d8218dcabe05c09a8d7a3f284f257
Robyn Felicity Heather Veitch <robynfhveitch@gnail.com>
2025-01-13 16:27:03 +0000

    feat: added a util file for common maths operations. added mean value calculation and standard deviation

---
+++ client/src/utils/mathsUtils.ts
@@ -0,0 +1,22 @@
+/**
+ * Gets a mean number for a list of numbers.
+ * @param nums List of numbers to find the mean of.
+ * @returns The mean.
+ */
+export const meanValue = (nums: number[]) => {
+	const n = nums.length;
+	return nums.reduce((a, e) => a + e, 0) / n;
+}
+
+/**
+ * Creates a standard deviation value for a given set of values.
+ * @param nums List of numbers to find the standard deviation of.
+ * @param subSample If `false`, the number list represents the entire sample size. If unsure leave as `true`.
+ * @returns 
+ */
+export const standardDeviation = (nums: number[], subSample: boolean = true) => {
+	const n = nums.length;
+	const m = meanValue(nums);
+	const numerator = nums.reduce((a, e) => a + Math.pow(e - m, 2), 0)
+	return Math.sqrt(numerator / (subSample ? (n - 1) : n))
+}
