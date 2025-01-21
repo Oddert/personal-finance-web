19ec0e71cd1a17251073265aeb816090c7c4cfa0
Robyn F H Veitch <oddert@gmail.com>
2025-01-21 22:57:46 +0000

    feat: added a catch-all error handling thunk to attempt to parse a range of common errors

--- src/redux/thunks/errorThunks.ts
+++ src/redux/thunks/errorThunks.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios';
 import { AppDispatch } from '../constants/store';
 
 import { IPayloadWriteError, writeError } from '../slices/errorSlice';
@@ -30,3 +31,75 @@ export const writeErrorBoundary =
             throw error;
         }
     };
+
+/**
+ * Ingests and attempts to parse a series of expected errors, with preference to particular error messages.
+ *
+ * Priority order:
+ * - On standard API responses, use our standard response object.
+ * - On failed API responses (e.g. network error or incorrect path 404), use Axios error attributes.
+ * - On all other errors inherited from the JS Error, use Error attributes.
+ * - Default back to a generic 'something went wrong' message.
+ * @category Redux
+ * @subcategory Thunks
+ * @param errorArgs Message components to override defaults.
+ */
+export const intakeError = (error: any) => async (dispatch: AppDispatch) => {
+    try {
+        if (
+            error instanceof AxiosError &&
+            error?.response?.data &&
+            !/<\!DOCTYPE html>/.test(error.response.data)
+        ) {
+            dispatch(
+                writeError(
+                    Object.assign(
+                        {
+                            title: 'Something went wrong',
+                            message: 'An unexpected error was encountered.',
+                            error: 'Application error',
+                            stackTrace: '',
+                        },
+                        {
+                            title: error.name,
+                            message: error.response.data.message,
+                            error: error.response.data.error,
+                            stackTrace: error.stack,
+                        },
+                    ),
+                ),
+            );
+        } else if (error instanceof Error) {
+            dispatch(
+                writeError(
+                    Object.assign(
+                        {
+                            title: 'Something went wrong',
+                            message: 'An unexpected error was encountered.',
+                            error: 'Application error',
+                            stackTrace: '',
+                        },
+                        {
+                            title: error.name,
+                            message: error.message,
+                            error: String(error),
+                            stackTrace: error.stack,
+                        },
+                    ),
+                ),
+            );
+        } else {
+            dispatch(
+                writeError({
+                    title: 'Something went wrong',
+                    message: 'An unexpected error was encountered.',
+                    error: 'Application error',
+                    stackTrace: '',
+                }),
+            );
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
