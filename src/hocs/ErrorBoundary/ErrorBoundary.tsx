646e44d887d1e03b8f32dc9f3704ddf62c18ce26
Robyn F H Veitch <oddert@gmail.com>
2025-01-22 14:58:24 +0000

    refactor: removed experimental error buondary code, re-implemented commented code

--- src/hocs/ErrorBoundary/ErrorBoundary.tsx
+++ src/hocs/ErrorBoundary/ErrorBoundary.tsx
@@ -1,73 +1,37 @@
-import { Component, JSX } from 'react';
-// import { Component, ErrorInfo, JSX } from 'react';
+import { Component, ErrorInfo, JSX } from 'react';
 
 interface IProps {
     children: JSX.Element | JSX.Element[] | null;
     fallback: JSX.Element | JSX.Element[] | null;
 }
 
-// interface IState {
-//     error: string | null;
-// }
+interface IState {
+    error: string | null;
+}
 
-// class ErrorBoundary extends Component<IProps, IState> {
-//     constructor(props: IProps) {
-//         super(props);
-//         this.state = {
-//             error: null,
-//         };
-//     }
-
-//     static getDerivedStateFromError(error: any) {
-//         console.log('getDerivedStateFromError');
-//         console.log(error);
-//         return {
-//             error: true,
-//         };
-//     }
-
-//     componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-//         console.log('componentDidCatch');
-//         console.error(error, errorInfo.componentStack);
-//     }
-
-//     render() {
-//         if (this.state.error) {
-//             console.log('fallback');
-//             return this.props.fallback;
-//         }
-//         console.log('children');
-//         return this.props.children;
-//     }
-// }
-
-class ErrorBoundary extends Component<IProps, { hasError: boolean }> {
+class ErrorBoundary extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
-        this.state = { hasError: false };
+        this.state = {
+            error: null,
+        };
     }
 
-    static getDerivedStateFromError(err: any) {
-        console.error('getDerivedStateFromError', err);
-        // Update state so the next render will show the fallback UI.
-        return { hasError: true };
+    static getDerivedStateFromError(error: any) {
+        console.error(error);
+        return {
+            error: true,
+        };
     }
 
-    componentDidCatch(error: any, info: any) {
-        // Example "componentStack":
-        //   in ComponentThatThrows (created by App)
-        //   in ErrorBoundary (created by App)
-        //   in div (created by App)
-        //   in App
-        console.error('componentDidCatch', error, info.componentStack);
+    componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+        console.error(error, errorInfo.componentStack);
     }
 
     render() {
-        if (this.state.hasError) {
-            // You can render any custom fallback UI
+        if (this.state.error) {
             return this.props.fallback;
         }
-
         return this.props.children;
     }
 }
