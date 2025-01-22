8529b3165d47dd76f49518a4b1e492f44fb3f3a7
Robyn F H Veitch <oddert@gmail.com>
2025-01-22 09:38:09 +0000

    security: added additional cases to generic error thunk

--- src/redux/thunks/errorThunks.ts
+++ src/redux/thunks/errorThunks.ts
@@ -46,30 +46,54 @@ export const writeErrorBoundary =
  */
 export const intakeError = (error: any) => async (dispatch: AppDispatch) => {
     try {
-        if (
-            error instanceof AxiosError &&
-            error?.response?.data &&
-            !/<\!DOCTYPE html>/.test(error.response.data)
-        ) {
-            dispatch(
-                writeError(
-                    Object.assign(
-                        {
-                            title: 'Something went wrong',
-                            message: 'An unexpected error was encountered.',
-                            error: 'Application error',
-                            stackTrace: '',
-                        },
-                        {
-                            title: error.name,
-                            message: error.response.data.message,
-                            error: error.response.data.error,
-                            stackTrace: error.stack,
-                        },
+        if (error instanceof AxiosError) {
+            // Condition 1: If a network error is caught we want to extract specific network info for the user.
+            if (
+                error?.response?.data &&
+                !/<\!DOCTYPE html>/.test(error.response.data)
+            ) {
+                // Condition 1.1: If the error is coming from our backend (request gets through at least), use our standard response format.
+                dispatch(
+                    writeError(
+                        Object.assign(
+                            {
+                                title: 'Something went wrong',
+                                message: 'An unexpected error was encountered.',
+                                error: 'Application error',
+                                stackTrace: '',
+                            },
+                            {
+                                title: error.name,
+                                message: error.response.data.message,
+                                error: error.response.data.error,
+                                stackTrace: error.stack,
+                            },
+                        ),
                     ),
-                ),
-            );
+                );
+            } else {
+                // Condition 1.2: If the request failed to reach our backend or an unhandled error occurs, use Axios's response info.
+                dispatch(
+                    writeError(
+                        Object.assign(
+                            {
+                                title: 'Something went wrong',
+                                message: 'An unexpected error was encountered.',
+                                error: 'Application error',
+                                stackTrace: '',
+                            },
+                            {
+                                title: error.name,
+                                message: error.message,
+                                error: error.cause,
+                                stackTrace: error.stack,
+                            },
+                        ),
+                    ),
+                );
+            }
         } else if (error instanceof Error) {
+            // Condition 2: Non network error but conforming to a standard JS error.
             dispatch(
                 writeError(
                     Object.assign(
@@ -89,6 +113,7 @@ export const intakeError = (error: any) => async (dispatch: AppDispatch) => {
                 ),
             );
         } else {
+            // Condition 3: No recognisable error format found.
             dispatch(
                 writeError({
                     title: 'Something went wrong',
@@ -99,7 +124,16 @@ export const intakeError = (error: any) => async (dispatch: AppDispatch) => {
             );
         }
     } catch (err) {
+        // Condition 4: Should any assumption in the 'try' block fail, create a generic error and log the contents.
         console.error(err);
-        throw err;
+        dispatch(
+            writeError({
+                title: 'Something went wrong and the application could not recover.',
+                message:
+                    'An unexpected error was encountered for which the application could not find a cause.',
+                error: 'Fatal Application error',
+                stackTrace: '',
+            }),
+        );
     }
 };
