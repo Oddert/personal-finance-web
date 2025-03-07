c673e3ce592d23d47726849019cbf24958215e33
Robyn F H Veitch <oddert@gmail.com>
2025-03-07 14:59:17 +0000

    docs: added docstrings to use token hook

--- src/hooks/useAuthToken.ts
+++ src/hooks/useAuthToken.ts
@@ -10,9 +10,21 @@ import { useAppDispatch } from './ReduxHookWrappers';
 
 const timeoutOffset = 1000 * 60 * 1;
 
+/**
+ * Handles authentication loading and refresh as part of application main lifecycle.
+ * @category Hooks
+ * @subcategory Use Auth Token
+ */
 const useAuthToken = () => {
     const dispatch = useAppDispatch();
 
+    /**
+     * Internal function to fetch the user details and authenticate them.
+     * @param accessToken The retrieved access token.
+     * @param decoded The decoded token body.
+     * @param callback Optional function called after the user has been successfully authenticated.
+     * @returns The result of the callback or null if none provided.
+     */
     // We'll allow underscore names to clearly show distinction for internal vs external methods.
     // eslint-disable-next-line @typescript-eslint/naming-convention
     const _authenticate = async (
@@ -42,11 +54,23 @@ const useAuthToken = () => {
         return callback ? callback() : null;
     };
 
+    /**
+     * In the event a queried access token is invalid, attempts to use the refresh token to query a new one.
+     *
+     * If this is not possible the user auth details are wiped and the user is directed to login again.
+     */
     const refreshToken = () => {
         // TODO: implement refresh tokens
         dispatch(userUnauthenticated());
     };
 
+    /**
+     * Checks the stored access token and either re-authenticates the user, or directs them to the login page.
+     *
+     * Intended for initial app loads.
+     * @param callback Optional function called after the user has been successfully authenticated.
+     * @returns The result of the callback function if provided or void.
+     */
     const conditionallyRefreshAuth = async (callback?: () => void) => {
         const accessToken = AuthLSService.getToken();
         if (!accessToken) {
@@ -64,7 +88,7 @@ const useAuthToken = () => {
 
             return await _authenticate(accessToken, decoded, callback);
         } catch (error) {
-            refreshToken();
+            return refreshToken();
         }
     };
 
