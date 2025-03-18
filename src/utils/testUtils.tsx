24ed8e699b470276e22308783a3de93b0cc6149f
Robyn F H Veitch <oddert@gmail.com>
2025-03-18 10:18:37 +0000

    fix: moved import location for localisation provider. updated test wrapper provider in line with main app provider

--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -1,27 +1,39 @@
 /* eslint-disable import/export */
-// import React, { JSX } from 'react';
-// import { Provider as ReduxProvider } from 'react-redux';
+import { JSXElementConstructor, ReactElement, ReactNode } from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 
-// import { RenderOptions, render } from '@testing-library/react';
-// import { ThemeProvider } from '@mui/material';
+import { RenderOptions, render } from '@testing-library/react';
+import { ThemeProvider } from '@mui/material';
 
-// import store from '../redux/constants/store';
-// import theme from '../theme';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
-// const Providers: React.FC<{ children: JSX.Element }> = ({ children }) => {
-//     if (!children) {
-//         return <p></p>;
-//     }
-//     return (
-//         <ReduxProvider store={store}>
-//             <ThemeProvider theme={theme}>{children}</ThemeProvider>
-//         </ReduxProvider>
-//     );
-// };
+import store from '../redux/constants/store';
+import theme from '../theme';
 
-// const customRenderer = (ui: React.ReactElement, options?: RenderOptions) =>
-//     render(ui, { wrapper: Providers, ...options });
+const Providers: JSXElementConstructor<{ children: ReactNode }> = ({
+    children,
+}) => {
+    if (!children) {
+        return <p></p>;
+    }
+    return (
+        <ReduxProvider store={store}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <ThemeProvider theme={theme}>
+                    {/* <ErrorBoundary fallback={<FallbackError />}>
+                        <ErrorMessage>{children}</ErrorMessage>
+                    </ErrorBoundary> */}
+                    {children}
+                </ThemeProvider>
+            </LocalizationProvider>
+        </ReduxProvider>
+    );
+};
+
+export const customRenderer = (ui: ReactElement, options?: RenderOptions) =>
+    render(ui, { wrapper: Providers, ...options });
 
 export * from '@testing-library/react';
 
-// export { customRenderer as render };
+export { customRenderer as render };
