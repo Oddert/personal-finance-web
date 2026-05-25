baab1c1de441343f116640628683992da7653b01
Robyn F H Veitch <oddert@gmail.com>
2026-05-25 14:44:52 +0100

    feat: moved scenario projection preview chart to use mui-x-charts. this rules. x-charts from now on. its so over for Apexcharts.

--- src/pages/EditScenario/components/ProjectionChart/ProjectionChart.tsx
+++ src/pages/EditScenario/components/ProjectionChart/ProjectionChart.tsx
@@ -1,7 +1,7 @@
 import { type FC, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { CircularProgress, Paper, useTheme } from '@mui/material';
+import { FormControlLabel, Paper, Switch, useTheme } from '@mui/material';
 import { BarChart } from '@mui/x-charts';
 
 import type { IProps } from './ProjectionChart.types';
@@ -12,19 +12,18 @@ import {
     useAppSelector,
 } from '../../../../hooks/ReduxHookWrappers';
 import { getActiveCardId } from '../../../../redux/selectors/cardSelectors';
-import { getCategoryOrderedDataById } from '../../../../redux/selectors/categorySelectors';
 import { intakeError } from '../../../../redux/thunks/errorThunks';
 import APIService from '../../../../services/APIService';
 
-const ProjectionChart: FC<IProps> = () => {
+const ProjectionChart: FC<IProps> = ({ previewMode }) => {
     const [pastData, setPastData] = useState<TAggregateDatapoints>({});
     const [pastDataLoading, setPastDataLoading] = useState(false);
+    const [showNegatives, setShowNegatives] = useState(true);
 
     const cardId = useAppSelector(getActiveCardId);
 
     const { t } = useTranslation();
     const dispatch = useAppDispatch();
-    const categoryLookup = useAppSelector(getCategoryOrderedDataById);
     const theme = useTheme();
 
     useEffect(() => {
@@ -38,7 +37,7 @@ const ProjectionChart: FC<IProps> = () => {
         const fetchPastData = async () => {
             try {
                 const pastDataResponse =
-                    await APIService.getAllTransactionsAggregated(cardId, true);
+                    await APIService.getAllTransactionsAggregated(cardId);
                 if (!pastDataResponse.payload) {
                     throw new Error(t('modalMessages.noServerResponse'));
                 }
@@ -53,47 +52,52 @@ const ProjectionChart: FC<IProps> = () => {
         fetchPastData();
     }, [dispatch, t, cardId]);
 
-    const monthKeys = useMemo(() => {
-        const months = new Set<string>();
-        Object.values(pastData).forEach((monthData) => {
-            monthData.forEach((month) => {
-                months.add(new Date(month.month).toISOString());
-            });
-        });
-        return Array.from(months).sort(
-            (a, b) => new Date(a).getTime() - new Date(b).getTime(),
-        );
-    }, [pastData]);
+    const { dataset, series } = useMemo(() => {
+        // With data pivoted on month the response format in pseudo code is: {[monthKey]: categoryData[]}
+        // Here we transform this into an array where each entry represents one month, with category IDs mapped to values.
+        // MUI X charts will use a series definition to resolve a label to these values.
+        // The accumulator uses top-level keys is for convenient lookup until the eventual Object.values() calls.
+        interface TLocalAcc {
+            // Record<monthKey, Record<categoryId, value>>
+            _dataset: Record<string, Record<string, string | number>>;
+            // Record<categoryId, seriesItem[]>
+            _series: Record<string, { label: string; dataKey: string }>;
+        }
 
-    const data = useMemo(
-        () =>
-            Object.entries(pastData).map(([categoryId, monthData]) => {
-                const monthMap = new Map(
-                    monthData.map((month) => [
-                        new Date(month.month).toISOString(),
-                        month,
-                    ]),
+        const { _dataset, _series } = Object.entries(pastData).reduce(
+            (monthAcc: TLocalAcc, [monthKey, categoryList]) => {
+                monthAcc._dataset[monthKey] = categoryList.reduce(
+                    (catAcc: Record<string, string | number>, category) => {
+                        const value = showNegatives
+                            ? category.totalDebit - category.totalCredit
+                            : category.totalDebit;
+                        if (!isNaN(value)) {
+                            catAcc[category.categoryId] = value;
+                        }
+                        if (!(category.categoryId in monthAcc._series)) {
+                            monthAcc._series[category.categoryId] = {
+                                dataKey: category.categoryId,
+                                label: category.categoryName,
+                            };
+                        }
+                        return catAcc;
+                    },
+                    {},
                 );
+                monthAcc._dataset[monthKey].month = monthKey;
+                return monthAcc;
+            },
+            { _dataset: {}, _series: {} },
+        );
 
-                return {
-                    series:
-                        categoryId in categoryLookup
-                            ? categoryLookup[categoryId].label
-                            : categoryId,
-                    data: monthKeys.map((monthKey) => {
-                        const month = monthMap.get(monthKey);
-                        return {
-                            x: monthKey,
-                            y: month ? month.totalDebit - month.totalCredit : 0,
-                        };
-                    }),
-                };
-            }),
-        [categoryLookup, monthKeys, pastData],
-    );
+        return {
+            dataset: Object.values(_dataset),
+            series: Object.values(_series),
+        };
+    }, [pastData, showNegatives]);
 
-    if (pastDataLoading) {
-        return <CircularProgress />;
+    if (previewMode === 'off') {
+        return null;
     }
 
     return (
@@ -104,7 +108,25 @@ const ProjectionChart: FC<IProps> = () => {
                 padding: '20px',
             }}
         >
-            <BarChart dataset={data} height={600} series={[]} />
+            <FormControlLabel
+                control={
+                    <Switch
+                        checked={showNegatives}
+                        onChange={(_, checked) => {
+                            setShowNegatives(checked);
+                        }}
+                    />
+                }
+                label='Include income'
+                sx={(theme) => ({ color: theme.palette.primary.contrastText })}
+            />
+            <BarChart
+                dataset={dataset}
+                height={600}
+                loading={pastDataLoading}
+                series={series}
+                xAxis={[{ dataKey: 'month' }]}
+            />
         </Paper>
     );
 };
